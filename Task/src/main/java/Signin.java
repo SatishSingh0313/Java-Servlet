cd3b3c85545579f14def482cc9c12520801a21db
SatishSingh0313 <satishsangam.205@gmail.com>
2024-03-24 19:53:27 +0530

    Login and Signup With Java Servlet

---
+++ Task/src/main/java/Signin.java
@@ -0,0 +1,59 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Signin")
+public class Signin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Signin() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	System.out.println("get");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	String name=request.getParameter("name");
+	String city=request.getParameter("city");
+	String email=request.getParameter("email");
+	String mobile=request.getParameter("mobile");
+	String password=request.getParameter("password");
+	
+	System.out.println(name);
+	System.out.println(city);
+	System.out.println(email);
+	System.out.println(mobile);
+	System.out.println(password);
+	
+	
+	 PrintWriter out=response.getWriter();
+	 out.println("<html><body bgcolor='blue'> Data Stored "+name+" "+city+" "+email+" "+mobile+" "+password);
+	 out.flush();
+	 
+	
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","root");
+		
+		Statement stmt =con.createStatement();
+		stmt.executeUpdate("insert into user values('"+name+"','"+city+"','"+email+"','"+mobile+"','"+password+"')");
+		con.close();
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+	}
+
+}
